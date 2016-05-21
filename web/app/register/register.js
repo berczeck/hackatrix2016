dfaaa3a8f165016018f0ea9383e1efa8eb899ead
elgiank <giancarlovr@gmail.com>
2016-05-21 17:03:10 -0500

    Conection with backend for isbn

--- web/app/register/register.js
+++ web/app/register/register.js
@@ -13,17 +13,18 @@ angular.module('myApp.register', ['ngRoute'])
   $scope.imageUrl = '';
   $scope.wantedBooks =[];
   $scope.booksFoundByIsbn = [];
+    $scope.hastIsbnResults = false;
     
     
   $scope.addWantedBook = function () {
     $scope.wantedBooks.push('');
   };
 
-  $scope.findBookByIsbn = function (){
-      $scope.booksFoundByIsbn = [
-          { name: 'Libro 1' }
-      ];
-  };
+  // $scope.findBookByIsbn = function (){
+  //     $scope.booksFoundByIsbn = [
+  //         { name: 'Libro 1' }
+  //     ];
+  // };
 
     $scope.selectBookFound =  function (index) {
         var bookInfo = $scope.booksFoundByIsbn[index];
@@ -37,12 +38,12 @@ angular.module('myApp.register', ['ngRoute'])
 
     $scope.saveData = function () {
         var data =  {
-            'imageUrl': $scope.imageUrl,
-            'isbn': $scope.isbn,
-            'title': $scope.title,
-            'author': $scope.locations,
-            'tags': $scope.tags.split(','),
-            'description': $scope.description,
+            'imageUrl': $scope.imageUrl || '',
+            'isbn': $scope.isbn || '',
+            'title': $scope.title || '',
+            'author': $scope.locations || '',
+            'tags': $scope.tags ? $scope.tags.split(',') : [],
+            'description': $scope.description || '',
             //'comments': [{}]
         };
 
@@ -54,17 +55,32 @@ angular.module('myApp.register', ['ngRoute'])
         $http.post('http://172.28.11.140/Hackatrix2016/api/books', data).then(successSave, errorSave);
 
         function successSave(result) {
-            console.log(result)
+            console.log(result);
+            $('#modalConfirmRegister').openModal();
         }
 
         function errorSave(errorStatus) {
-            console.log(errorStatus)
+            console.log(errorStatus);
             console.log("Error");
         }
     }
     
-    function getBookByIsbn(){
-        
+    $scope.findBookByIsbn = function (){
+        //9781451648546
+        $http.post('http://172.28.11.140/Hackatrix2016/api/books/find?isbn=' + $scope.isbn).then(successSave1, errorSave1);
+
+        function successSave1(result) {
+            console.log(result);
+            if(result.length > 0){
+                $scope.hastIsbnResults = true;
+                $scope.booksFoundByIsbn = result;
+            }
+        }
+
+        function errorSave1(errorStatus) {
+            console.log(errorStatus);
+            console.log("Error");
+        }
     }
     
 }]);
