2abc5cabeefa8939538907c62c25c7127b3c0da8
berzceck <berczeck_400@hotmail.com>
2016-05-21 10:39:11 -0500

    Api solution

---
+++ webapi/Api/Api/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Api
+{
+    public class HomeController : ApiController
+    {
+        public string GetVersion()
+        {
+            return "1.1.0";
+        }
+    }
+}
