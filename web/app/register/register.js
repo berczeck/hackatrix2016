45ded1ef6a9211b386cc56ff6d4b453a72556468
elgiank <giancarlovr@gmail.com>
2016-05-21 13:19:28 -0500

    Post register

--- web/app/register/register.js
+++ web/app/register/register.js
@@ -9,6 +9,21 @@ angular.module('myApp.register', ['ngRoute'])
   });
 }])
 
-.controller('RegisterCrtl', [function() {
+.controller('RegisterCrtl', ['$scope', function($scope) {
+  // $scope.imageUrl = imgUrl;
+  // $scope.title = title;
+  // $scope.isbn = isbn;
+  // $scope.description =  description;
+  // $scope.tags = Array.isArray(tags) ? tags : [];
+  // $scope.wantedBooks = Array.isArray(books) ? books : [];
+  
+  /*function Post(imgUrl, title, isbn, description, tags, books) {
+    this.imageUrl = imgUrl;
+    this.title = title;
+    this.isbn = isbn;
+    this.description =  description;
+    this.tags = Array.isArray(tags) ? tags : [];
+    this.wantedBooks = Array.isArray(books) ? books : [];
+  }*/
 
 }]);
