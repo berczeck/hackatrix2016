8b0409e56dd1c019f30ae2cd16973af7cb0f8cfd
elgiank <giancarlovr@gmail.com>
2016-05-21 15:05:48 -0500

    Post register changes

--- web/app/register/register.js
+++ web/app/register/register.js
@@ -9,13 +9,31 @@ angular.module('myApp.register', ['ngRoute'])
   });
 }])
 
-.controller('RegisterCrtl', ['$scope', function($scope) {
+.controller('RegisterCrtl', ['$scope', '$timeout', function($scope, $timeout) {
   $scope.imageUrl = '';
   $scope.wantedBooks =[];
+  $scope.booksFoundByIsbn = [];
+    
+    
   $scope.addWantedBook = function () {
     $scope.wantedBooks.push('');
   };
 
-  
+  $scope.findBookByIsbn = function (){
+      $scope.booksFoundByIsbn = [
+          { name: 'Libro 1' },
+          { name: 'Libro 2' }
+      ];
+    }
+
+    $scope.selectBookFound =  function (index) {
+        var bookInfo = $scope.booksFoundByIsbn[index];
+        $scope.title = bookInfo.name;
+        $scope.booksFoundByIsbn =[];
+        $timeout(function () {
+            Materialize.updateTextFields();
+        }, 200)
+
+    }
 
 }]);
