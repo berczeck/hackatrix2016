4a7f2371ce0ee40e6ed7623322da1942e60d65e8
Fernando Catacora <nando.drw@gmail.com>
2016-05-21 16:31:18 -0500

    rc

--- web/app/lookfor/lookfor.js
+++ web/app/lookfor/lookfor.js
@@ -9,38 +9,56 @@ angular.module('myApp.lookfor', ['ngRoute'])
   });
 }])
 
-.controller('LookforCrtl', ['$scope','$location', function($scope, $location) {
+.controller('LookforCrtl', ['$scope','$location', '$http', function($scope, $location, $http) {
 
-  $scope.books = [
-    {
-      // img: 'http://akamaicovers.oreilly.com/images/0636920035848/cat.gif',
-      img: 'assets/book.gif',
-      name: 'Libro 1',
-      holder: 'juan',
-      isbn: '234343FG'
-    },
-    {
-      // img: 'http://akamaicovers.oreilly.com/images/0636920035848/cat.gif',
-      img: 'assets/book.gif',
-      name: 'Libro 2',
-      holder: 'pedro',
-      isbn: 'dfdsfsd23213'
-    },
-    {
-      // img: 'http://akamaicovers.oreilly.com/images/0636920035848/cat.gif',
-      img: 'assets/book.gif',
-      name: 'Libro 3',
-      holder: 'jhon',
-      isbn: 'dfdsfsd23213'
-    },
-    {
-      // img: 'http://akamaicovers.oreilly.com/images/0636920035848/cat.gif',
-      img: 'assets/book.gif',
-      name: 'Libro 4',
-      holder: 'lucho',
-      isbn: 'dfdsfsd23213'
+  $scope.books = [];
+
+  $http.get('http://172.28.11.140/Hackatrix2016/api/books/all')
+
+  .then(function (response) {
+
+    for (var i = response.data.length - 1; i >= 0; i--) {
+      $scope.books.push({
+        img: response.data[i].imageUrl,
+        name: response.data[i].title,
+        holder: 'juan',
+        isbn: response.data[i].isbn
+      });
+      // debugger;
     }
-  ];
+  });
+
+
+  // $scope.books = [
+  //   {
+  //     // img: 'http://akamaicovers.oreilly.com/images/0636920035848/cat.gif',
+  //     img: 'assets/book.gif',
+  //     name: 'Libro 1',
+  //     holder: 'juan',
+  //     isbn: '234343FG'
+  //   },
+  //   {
+  //     // img: 'http://akamaicovers.oreilly.com/images/0636920035848/cat.gif',
+  //     img: 'assets/book.gif',
+  //     name: 'Libro 2',
+  //     holder: 'pedro',
+  //     isbn: 'dfdsfsd23213'
+  //   },
+  //   {
+  //     // img: 'http://akamaicovers.oreilly.com/images/0636920035848/cat.gif',
+  //     img: 'assets/book.gif',
+  //     name: 'Libro 3',
+  //     holder: 'jhon',
+  //     isbn: 'dfdsfsd23213'
+  //   },
+  //   {
+  //     // img: 'http://akamaicovers.oreilly.com/images/0636920035848/cat.gif',
+  //     img: 'assets/book.gif',
+  //     name: 'Libro 4',
+  //     holder: 'lucho',
+  //     isbn: 'dfdsfsd23213'
+  //   }
+  // ];
 
   $scope.lookForBook = '';
 
