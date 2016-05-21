419813ef79dcd9441afd86f117562c5ddcf3cf2f
berzceck <berczeck_400@hotmail.com>
2016-05-21 11:09:00 -0500

    book

--- webapi/Api/Models/Book.cs
+++ webapi/Api/Models/Book.cs
@@ -7,5 +7,11 @@ namespace Api.Models
 {
     public class Book
     {
+        public string Genero { get; set; }
+        public string Codigo { get; set; }
+        public string  Autor { get; set; }
+        public string[] MyProperty { get; set; }
     }
 }
+
+//{[]}
--- webapi/Api/Repo/BookRepository.cs
+++ webapi/Api/Repo/BookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,16 @@ namespace Api.Repo
 {
     public class BookRepository
     {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        static BookRepository()
+        {
+            _client = new MongoClient("mongodb://localhost:27017/");
+            _database = _client.GetDatabase("book");
+        }
+
+
+
     }
 }
